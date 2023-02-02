91f2c8e8765bbe8ff65a6863cad194df3a7bd859
Ashish Jade <jadeashish@gmail.com>
2023-02-02 19:14:55 +0530

    check in

---
+++ JwtWebAPI/Models/UserConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtWebAPI.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "jadeashish", EmailAddress = "jadeashish@email.com", Password = "password123", GivenName = "Ashish", Surname = "Jade", Role = "Administrator" },
+            new UserModel() { Username = "jademeer", EmailAddress = "jademeer@email.com", Password = "password321", GivenName = "Meer", Surname = "Jade", Role = "Seller" },
+        };
+    }
+}
